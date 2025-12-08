e25c6cf30228cc70c6665a0e7f49ba480a128279
felixcp <145021906+felixcaronpare@users.noreply.github.com>
2025-12-08 14:51:22 -0500

    ui changes

--- core/management/commands/setup_desks.py
+++ core/management/commands/setup_desks.py
@@ -19,19 +19,19 @@ class Command(BaseCommand):
             {"number" : "6", "name": "Desk 6", "x": 75, "y": 15},
 
             # Middle Left Block
-            {"number" : "7", "name": "Desk 7", "x": 34.5, "y": 44},
-            {"number" : "8", "name": "Desk 8", "x": 49.5, "y": 44},
-            {"number" : "9", "name": "Desk 9", "x": 34.5, "y": 51},
-            {"number" : "10", "name": "Desk 10", "x": 49.5, "y": 51},
+            {"number" : "13", "name": "Desk 13", "x": 34.5, "y": 44},
+            {"number" : "14", "name": "Desk 14", "x": 49.5, "y": 44},
+            {"number" : "15", "name": "Desk 15", "x": 34.5, "y": 51},
+            {"number" : "16", "name": "Desk 16", "x": 49.5, "y": 51},
 
             # Middle right area
-            {"number" : "11", "name": "Desk 11", "x": 76, "y": 22.5},
-            {"number" : "12", "name": "Desk 12", "x": 91, "y": 22.5},
-            {"number" : "13", "name": "Desk 13", "x": 76, "y": 29.5},
-            {"number" : "14", "name": "Desk 14", "x": 91, "y": 29.5},
+            {"number" : "7", "name": "Desk 7", "x": 76, "y": 22.5},
+            {"number" : "8", "name": "Desk 8", "x": 91, "y": 22.5},
+            {"number" : "9", "name": "Desk 9", "x": 76, "y": 29.5},
+            {"number" : "10", "name": "Desk 10", "x": 91, "y": 29.5},
 
-            {"number" : "15", "name": "Desk 15", "x": 76, "y": 37},
-            {"number" : "16", "name": "Desk 16", "x": 91, "y": 37},
+            {"number" : "11", "name": "Desk 11", "x": 76, "y": 37},
+            {"number" : "12", "name": "Desk 12", "x": 91, "y": 37},
 
             {"number" : "17", "name": "Desk 17", "x": 76, "y": 44},
             {"number" : "18", "name": "Desk 18", "x": 91, "y": 44},
